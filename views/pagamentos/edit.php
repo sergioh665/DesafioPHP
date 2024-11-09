5b5b5bf83199eafa29769761a8e0c15b61f385c7
sergioh665 <sergioh665@gmail.com>
2024-11-09 18:00:30 -0300

    🔧CHORE: enviando views da classe pagamento

---
+++ views/pagamentos/edit.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../../src/Router.php';
+$pagamento = $pagamento->read($_GET['id']);
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Pagamento</title>
+</head>
+<body>
+    <h1>Editar Pagamento</h1>
+    <form action="/pagamentos/update" method="post">
+        <input type="hidden" name="id" value="<?= $pagamento['id'] ?>">
+        <label for="associado_id">Associado ID:</label>
+        <input type="number" id="associado_id" name="associado_id" value="<?= $pagamento['associado_id'] ?>" required><br>
+        <label for="anuidade_id">Anuidade ID:</label>
+        <input type="number" id="anuidade_id" name="anuidade_id" value="<?= $pagamento['anuidade_id'] ?>" required><br>
+        <label for="pago">Pago:</label>
+        <input type="checkbox" id="pago" name="pago" <?= $pagamento['pago'] ? 'checked' : '' ?>><br>
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
