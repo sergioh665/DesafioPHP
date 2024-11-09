8b430c78186fb2e2a3b0b9ba4a5d5e3c48a6171e
sergioh665 <sergioh665@gmail.com>
2024-11-09 16:33:55 -0300

    🧩CHORE: conectando com o banco de dados

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db = 'devs_do_rn';
+$user = 'root';
+$pass = '';
+
+// tenta conectar ao banco de dados
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Não foi possível conectar ao banco de dados $db :" . $e->getMessage());
+}
+?>
