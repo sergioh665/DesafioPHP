0dc538044f14bd9e03f8de56ab69be8b95bacb67
sergioh665 <sergioh665@gmail.com>
2024-11-10 08:51:33 -0300

    🔧CHORE: adicionando target=_blank ao  index.php

--- views/index.php
+++ views/index.php
@@ -8,9 +8,9 @@
     <h1>Bem-vindo ao Sistema de Gerenciamento de Associados</h1>
     <nav>
         <ul>
-            <li><a href="/associados">Associados</a></li>
-            <li><a href="/anuidades">Anuidades</a></li>
-            <li><a href="/pagamentos">Pagamentos</a></li>
+            <li><a href="/associados" target="_blank">Associados</a></li>
+            <li><a href="/anuidades" target="_blank">Anuidades</a></li>
+            <li><a href="/pagamentos" target="_blank">Pagamentos</a></li>
         </ul>
     </nav>
 </body>
