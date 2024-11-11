f086c65162857675911db9036e38160c1bf49a9e
sergioh665 <sergioh665@gmail.com>
2024-11-11 20:26:04 -0300

    🔧CHORE: criando header e footer para padronizar css

---
+++ views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Devs do RN</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="icon" href="/images/icon.png" type="image/png">
+</head>
+<body>
+    <header>
+        <a href="/"><img src="/images/banner.png" alt="Banner clicável"></a>
+    </header>
+    <div class="container">
