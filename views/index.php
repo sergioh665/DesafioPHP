5dd97d482a0668eb19ffd1bde087fd901b383b32
sergioh665 <sergioh665@gmail.com>
2024-11-10 22:35:43 -0300

    🧹CHORE: removendo target="_blank" e adcionando div

--- views/index.php
+++ views/index.php
@@ -3,15 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Devs do RN</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="icon" href="/images/icon.png" type="image/png">
 </head>
 <body>
-    <h1>Bem-vindo ao Sistema de Gerenciamento de Associados</h1>
-    <nav>
-        <ul>
-            <li><a href="/associados" target="_blank">Associados</a></li>
-            <li><a href="/anuidades" target="_blank">Anuidades</a></li>
-            <li><a href="/pagamentos" target="_blank">Pagamentos</a></li>
-        </ul>
-    </nav>
+    <header>
+        <a href="/"><img src="/images/banner.png" alt="Banner clicável"></a>
+    </header>
+    <div class="container">
+        <h1>Bem-vindo ao Sistema de Gerenciamento de Associados</h1>
+        <nav>
+            <ul>
+                <li><a href="/associados">Associados</a></li>
+                <li><a href="/anuidades">Anuidades</a></li>
+                <li><a href="/pagamentos">Pagamentos</a></li>
+            </ul>
+        </nav>
+    </div>
 </body>
 </html>
